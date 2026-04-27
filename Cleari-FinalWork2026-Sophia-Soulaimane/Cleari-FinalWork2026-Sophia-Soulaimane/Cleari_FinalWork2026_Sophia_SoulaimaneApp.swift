7791579061ff34e410785c7b92812f30112bbc89
Soulaimane Saadi <146191762+SoulaimaneSaadi@users.noreply.github.com>
2026-04-27 11:56:34 +0200

    feat(auth): create authentication background component

--- Cleari-FinalWork2026-Sophia-Soulaimane/Cleari-FinalWork2026-Sophia-Soulaimane/Cleari_FinalWork2026_Sophia_SoulaimaneApp.swift
+++ Cleari-FinalWork2026-Sophia-Soulaimane/Cleari-FinalWork2026-Sophia-Soulaimane/Cleari_FinalWork2026_Sophia_SoulaimaneApp.swift
@@ -18,7 +18,7 @@ struct Cleari_FinalWork2026_Sophia_SoulaimaneApp: App {
 
     var body: some Scene {
         WindowGroup {
-            FeedView()
+            AuthBackground()
                 .environment(\.managedObjectContext, persistenceController.container.viewContext)
         }
     }
--- Cleari-FinalWork2026-Sophia-Soulaimane/Cleari-FinalWork2026-Sophia-Soulaimane/Features/Auth/Components/AuthBackground.swift
+++ Cleari-FinalWork2026-Sophia-Soulaimane/Cleari-FinalWork2026-Sophia-Soulaimane/Features/Auth/Components/AuthBackground.swift
@@ -5,3 +5,22 @@
 //  Created by Soulaimane Saadi on 27/04/2026.
 //
 
+import SwiftUI
+
+struct AuthBackground: View {
+    var body: some View {
+        ZStack {
+            Image("authBackground")
+                .resizable()
+                .scaledToFill()
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                .ignoresSafeArea()
+
+            Color(hex: "C66F8C")
+                .opacity(0.45)
+                .ignoresSafeArea()
+        }
+    }
+}
+
+
