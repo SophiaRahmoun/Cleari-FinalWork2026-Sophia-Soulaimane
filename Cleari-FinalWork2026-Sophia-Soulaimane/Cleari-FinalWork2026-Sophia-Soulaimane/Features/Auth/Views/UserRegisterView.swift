b6b2bf33b637ed226ebcaad088cda82883949f40
Soulaimane Saadi <146191762+SoulaimaneSaadi@users.noreply.github.com>
2026-04-27 15:59:40 +0200

    feat(auth): create UserRegisterView

--- Cleari-FinalWork2026-Sophia-Soulaimane/Cleari-FinalWork2026-Sophia-Soulaimane/Features/Auth/Views/UserRegisterView.swift
+++ Cleari-FinalWork2026-Sophia-Soulaimane/Cleari-FinalWork2026-Sophia-Soulaimane/Features/Auth/Views/UserRegisterView.swift
@@ -5,3 +5,82 @@
 //  Created by admin on 15/02/2026.
 //
 
+import SwiftUI
+
+struct UserRegisterView: View {
+    @State private var firstName = ""
+    @State private var lastName = ""
+    @State private var email = ""
+    @State private var password = ""
+
+    @State private var selectedMonth = ""
+    @State private var selectedDay = ""
+    @State private var selectedYear = ""
+
+    var body: some View {
+        ZStack {
+            RadialGradientBackground(
+                startHex: "C66F8C",
+                endHex: "F9BDB9"
+            )
+            VStack(spacing: 24) {
+                TypographyLabel(
+                    text: "Create your account",
+                    style: .h1Italic,
+                    color: .black
+                )
+                .padding(.top, 85)
+
+                AuthRegisterInput(
+                    label: "Voornaam",
+                    text: $firstName,
+                    maxLength: 50
+                )
+
+                AuthRegisterInput(
+                    label: "Achternaam",
+                    text: $lastName,
+                    maxLength: 50
+                )
+
+                AuthRegisterInput(
+                    label: "Email",
+                    text: $email,
+                    maxLength: 50
+                )
+
+                AuthRegisterInput(
+                    label: "Password",
+                    text: $password,
+                    maxLength: 50
+                )
+
+                AuthBirthdatePicker(
+                    selectedMonth: $selectedMonth,
+                    selectedDay: $selectedDay,
+                    selectedYear: $selectedYear
+                )
+
+                PrimaryButton(title: "NEXT STEP") {
+                    print("User register tapped")
+                }
+                .padding(.horizontal, 60)
+                .padding(.top, 10)
+
+                AuthBottomLink(
+                    text: "Already have an account?",
+                    linkText: "Sign in"
+                )
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .padding(.top, 18)
+
+                Spacer()
+            }
+            .padding(.horizontal, 32)
+        }
+    }
+}
+
+#Preview {
+    UserRegisterView()
+}
