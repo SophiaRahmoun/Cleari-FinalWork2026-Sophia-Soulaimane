780a11c28c2a40cb8f18031f04e152545f1dea0d
Soulaimane Saadi <146191762+SoulaimaneSaadi@users.noreply.github.com>
2026-04-28 11:15:28 +0200

    feat(scan): create scan view model for camera control

--- Cleari-FinalWork2026-Sophia-Soulaimane/Cleari-FinalWork2026-Sophia-Soulaimane/Features/Scan/ViewModels/ScanViewModel.swift
+++ Cleari-FinalWork2026-Sophia-Soulaimane/Cleari-FinalWork2026-Sophia-Soulaimane/Features/Scan/ViewModels/ScanViewModel.swift
@@ -5,3 +5,86 @@
 //  Created by Soulaimane Saadi on 15/02/2026.
 //
 
+import SwiftUI
+import AVFoundation
+
+final class ScanViewModel: NSObject, ObservableObject {
+
+    let cameraSession = AVCaptureSession()
+    let photoOutput = AVCapturePhotoOutput()
+
+    func checkCameraPermission() {
+        switch AVCaptureDevice.authorizationStatus(for: .video) {
+
+        case .authorized:
+            setupCamera()
+            print("setupCamera started")
+
+        case .notDetermined:
+            AVCaptureDevice.requestAccess(for: .video) { granted in
+                if granted {
+                    DispatchQueue.main.async {
+                        self.setupCamera()
+                    }
+                }
+            }
+
+        default:
+            print("Camera permission denied")
+        }
+    }
+
+    private func setupCamera() {
+        cameraSession.beginConfiguration()
+        cameraSession.sessionPreset = .photo
+
+        guard let camera = AVCaptureDevice.default(.builtInWideAngleCamera, for: .video, position: .back),
+              let cameraInput = try? AVCaptureDeviceInput(device: camera),
+              cameraSession.canAddInput(cameraInput),
+              cameraSession.canAddOutput(photoOutput) else {
+            return
+        }
+
+        cameraSession.addInput(cameraInput)
+        cameraSession.addOutput(photoOutput)
+
+        cameraSession.commitConfiguration()
+
+        DispatchQueue.global(qos: .userInitiated).async {
+            self.cameraSession.startRunning()
+            print("camera starting")
+        }
+    }
+
+    func takePhoto() {
+        let settings = AVCapturePhotoSettings()
+        photoOutput.capturePhoto(with: settings, delegate: self)
+    }
+
+    func stopCamera() {
+        if cameraSession.isRunning {
+            cameraSession.stopRunning()
+        }
+    }
+}
+
+extension ScanViewModel: AVCapturePhotoCaptureDelegate {
+
+    func photoOutput(
+        _ output: AVCapturePhotoOutput,
+        didFinishProcessingPhoto photo: AVCapturePhoto,
+        error: Error?
+    ) {
+        if let error = error {
+            print("Photo error:", error)
+            return
+        }
+
+        guard let imageData = photo.fileDataRepresentation(),
+              let image = UIImage(data: imageData) else {
+            return
+        }
+
+        print("Photo taken:", image)
+    }
+}
