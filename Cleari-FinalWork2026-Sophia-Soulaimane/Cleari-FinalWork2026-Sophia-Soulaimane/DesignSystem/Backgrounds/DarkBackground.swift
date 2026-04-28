69364230fa4e52184acee55b321cee51dc2923cc
Soulaimane Saadi <146191762+SoulaimaneSaadi@users.noreply.github.com>
2026-04-28 16:45:14 +0200

    feat(profile): add dark background and restructure profile components

---
+++ Cleari-FinalWork2026-Sophia-Soulaimane/Cleari-FinalWork2026-Sophia-Soulaimane/DesignSystem/Backgrounds/DarkBackground.swift
@@ -0,0 +1,15 @@
+//
+//  DarkBackground.swift
+//  Cleari-FinalWork2026-Sophia-Soulaimane
+//
+//  Created by Soulaimane Saadi on 28/04/2026.
+//
+
+import SwiftUI
+
+struct DarkBackground: View {
+    var body: some View {
+        Color(hex: "1A1018")
+            .ignoresSafeArea()
+    }
+}
