493d0b29d2e66bbc0387e7ea36af3ad3ef2432e9
Soulaimane Saadi <146191762+SoulaimaneSaadi@users.noreply.github.com>
2026-04-28 16:56:30 +0200

    feat(ui): create avatar view component

--- Cleari-FinalWork2026-Sophia-Soulaimane/Cleari-FinalWork2026-Sophia-Soulaimane/DesignSystem/Components/AvatarView.swift
+++ Cleari-FinalWork2026-Sophia-Soulaimane/Cleari-FinalWork2026-Sophia-Soulaimane/DesignSystem/Components/AvatarView.swift
@@ -5,3 +5,17 @@
 //  Created by admin on 15/02/2026.
 //
 
+import SwiftUI
+
+struct AvatarView: View {
+    let imageName: String
+    var size: CGFloat = 110
+
+    var body: some View {
+        Image(imageName)
+            .resizable()
+            .scaledToFill()
+            .frame(width: size, height: size)
+            .clipShape(Circle())
+    }
+}
