ce1f70f31ccdc87cf149ef8995e1afb914269290
Soulaimane Saadi <146191762+SoulaimaneSaadi@users.noreply.github.com>
2026-04-28 21:25:20 +0200

    feat(profile): structure profile sections with titles

--- Cleari-FinalWork2026-Sophia-Soulaimane/Cleari-FinalWork2026-Sophia-Soulaimane/Features/Profile/Components/ProfileMenuRow.swift
+++ Cleari-FinalWork2026-Sophia-Soulaimane/Cleari-FinalWork2026-Sophia-Soulaimane/Features/Profile/Components/ProfileMenuRow.swift
@@ -5,3 +5,23 @@
 //  Created by Soulaimane Saadi on 28/04/2026.
 //
 
+import SwiftUI
+
+struct ProfileMenuRow: View {
+    let title: String
+    var isDestructive: Bool = false
+
+    var body: some View {
+        HStack {
+            Text(title)
+                .font(AppFont.gillSwiftUI(.regular, size: 18))
+                .foregroundColor(isDestructive ? Color.pink : .white)
+
+            Spacer()
+
+            Image(systemName: "chevron.right")
+                .foregroundColor(.white.opacity(0.6))
+        }
+        .padding(.vertical, 10)
+    }
+}
--- Cleari-FinalWork2026-Sophia-Soulaimane/Cleari-FinalWork2026-Sophia-Soulaimane/Features/Profile/Components/ProfileMenuSection.swift
+++ Cleari-FinalWork2026-Sophia-Soulaimane/Cleari-FinalWork2026-Sophia-Soulaimane/Features/Profile/Components/ProfileMenuSection.swift
@@ -5,3 +5,16 @@
 //  Created by Soulaimane Saadi on 28/04/2026.
 //
 
+import SwiftUI
+
+struct ProfileMenuSection: View {
+    let title: String
+
+    var body: some View {
+        Text(title)
+            .font(AppFont.gillSwiftUI(.bold, size: 28))
+            .foregroundColor(.white)
+            .frame(maxWidth: .infinity, alignment: .leading)
+            .padding(.top, 22)
+    }
+}
