5a702c106e316a73ff75c1879badfa1433be83d7
Soulaimane Saadi <146191762+SoulaimaneSaadi@users.noreply.github.com>
2026-04-28 21:23:48 +0200

    feat(profile): add ProfileStat component

--- Cleari-FinalWork2026-Sophia-Soulaimane/Cleari-FinalWork2026-Sophia-Soulaimane/Features/Profile/Components/ProfileStat.swift
+++ Cleari-FinalWork2026-Sophia-Soulaimane/Cleari-FinalWork2026-Sophia-Soulaimane/Features/Profile/Components/ProfileStat.swift
@@ -5,3 +5,21 @@
 //  Created by Soulaimane Saadi on 28/04/2026.
 //
 
+import SwiftUI
+
+struct ProfileStat: View {
+    let number: String
+    let label: String
+
+    var body: some View {
+        VStack(spacing: 4) {
+            Text(number)
+                .font(AppFont.gillSwiftUI(.bold, size: 18))
+                .foregroundColor(.white)
+
+            Text(label)
+                .font(AppFont.gillSwiftUI(.bold, size: 16))
+                .foregroundColor(.white)
+        }
+    }
+}
