ccbb432180103fd76cb94a7d453c7a5b18d904b3
Soulaimane Saadi <146191762+SoulaimaneSaadi@users.noreply.github.com>
2026-04-28 12:18:14 +0200

    feat(ui): add reusable background components

---
+++ Cleari-FinalWork2026-Sophia-Soulaimane/Cleari-FinalWork2026-Sophia-Soulaimane/DesignSystem/Backgrounds/BeigeBackground.swift
@@ -0,0 +1,15 @@
+//
+//  BeigeBackground.swift
+//  Cleari-FinalWork2026-Sophia-Soulaimane
+//
+//  Created by Soulaimane Saadi on 28/04/2026.
+//
+
+import SwiftUI
+
+struct BeigeBackground: View {
+    var body: some View {
+        Color("AccentColor")
+            .ignoresSafeArea()
+    }
+}
