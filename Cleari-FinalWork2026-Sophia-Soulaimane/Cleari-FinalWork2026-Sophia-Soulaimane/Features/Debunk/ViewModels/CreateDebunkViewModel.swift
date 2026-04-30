222a2a3d583ec9a4d1663971d9ed0ea8a810c5b4
Soulaimane Saadi <146191762+SoulaimaneSaadi@users.noreply.github.com>
2026-04-30 14:39:37 +0200

    refactor(debunk): remove duplicate files
    Deleted duplicated debunk files and cleaned project structure

--- Cleari-FinalWork2026-Sophia-Soulaimane/Cleari-FinalWork2026-Sophia-Soulaimane/Features/Debunk/ViewModels/CreateDebunkViewModel.swift
+++
@@ -1,7 +0,0 @@
-//
-//  CreateDebunkViewModel.swift
-//  Cleari-FinalWork2026-Sophia-Soulaimane
-//
-//  Created by admin on 15/02/2026.
-//
-
--- Cleari-FinalWork2026-Sophia-Soulaimane/Cleari-FinalWork2026-Sophia-Soulaimane/Features/Debunk/Views/DebunkDetailView.swift
+++
@@ -1,9 +0,0 @@
-//
-//  DebunkDetailView.swift
-//  Cleari-FinalWork2026-Sophia-Soulaimane
-//
-//  Created by admin on 15/02/2026.
-//
-
-
-
