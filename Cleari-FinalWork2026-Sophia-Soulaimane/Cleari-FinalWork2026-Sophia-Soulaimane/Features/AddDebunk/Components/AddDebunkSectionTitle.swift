077451ea851af177a145c3670a277a9f8112a73d
Soulaimane Saadi <146191762+SoulaimaneSaadi@users.noreply.github.com>
2026-05-01 11:37:56 +0200

    feat(debunk): refine AddDebunk UI components

--- Cleari-FinalWork2026-Sophia-Soulaimane/Cleari-FinalWork2026-Sophia-Soulaimane/Features/AddDebunk/Components/AddDebunkSectionTitle.swift
+++ Cleari-FinalWork2026-Sophia-Soulaimane/Cleari-FinalWork2026-Sophia-Soulaimane/Features/AddDebunk/Components/AddDebunkSectionTitle.swift
@@ -5,3 +5,18 @@
 //  Created by Soulaimane Saadi on 01/05/2026.
 //
 
+import SwiftUI
+
+struct AddDebunkSectionTitle: View {
+    let title: String
+
+    init(_ title: String) {
+        self.title = title
+    }
+
+    var body: some View {
+        Text(title)
+            .font(AppFont.gillSwiftUI(.regular, size: 20))
+            .foregroundColor(Color(hex: "1A1018"))
+    }
+}
