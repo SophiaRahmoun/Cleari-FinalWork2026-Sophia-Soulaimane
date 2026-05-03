7a28a547ab9da0c82dd36f76d7d64a97304b08da
Soulaimane Saadi <146191762+SoulaimaneSaadi@users.noreply.github.com>
2026-05-03 16:24:10 +0200

    feat: integrate ConsultationFormView as main entry

--- Cleari-FinalWork2026-Sophia-Soulaimane/Cleari-FinalWork2026-Sophia-Soulaimane/Features/Forms/Views/ConsultationFormView.swift
+++ Cleari-FinalWork2026-Sophia-Soulaimane/Cleari-FinalWork2026-Sophia-Soulaimane/Features/Forms/Views/ConsultationFormView.swift
@@ -5,3 +5,26 @@
 //  Created by Soulaimane Saadi on 15/02/2026.
 //
 
+import SwiftUI
+
+struct ConsultationFormView: View {
+    @StateObject private var viewModel = ConsultationFormViewModel()
+
+    var body: some View {
+        Group {
+            switch viewModel.currentStep {
+            case 1:
+                KnowYourSkinView(viewModel: viewModel)
+
+            case 2:
+                YourSkinHistoryView(viewModel: viewModel)
+
+            case 3:
+                YourSkinTodayView(viewModel: viewModel)
+
+            default:
+                KnowYourSkinView(viewModel: viewModel)
+            }
+        }
+    }
+}
