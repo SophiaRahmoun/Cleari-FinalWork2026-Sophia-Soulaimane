81d5ec1aad3b8397f5d285efa9c873d3f6b2f3f4
Soulaimane Saadi <146191762+SoulaimaneSaadi@users.noreply.github.com>
2026-05-03 15:46:35 +0200

    feat: add ConsultationFormData model

--- Cleari-FinalWork2026-Sophia-Soulaimane/Cleari-FinalWork2026-Sophia-Soulaimane/Features/Forms/Models/ConsultationFormData.swift
+++ Cleari-FinalWork2026-Sophia-Soulaimane/Cleari-FinalWork2026-Sophia-Soulaimane/Features/Forms/Models/ConsultationFormData.swift
@@ -5,3 +5,45 @@
 //  Created by Soulaimane Saadi on 03/05/2026.
 //
 
+// stock the answers from the form
+
+import Foundation
+
+struct ConsultationFormData: Codable {
+    var skinFeeling: String?
+    var productReaction: String?
+    var flakiness: String?
+
+    var diagnosedCondition: String?
+    var hasAllergies: String?
+    var allergiesDetails: String
+    var hasSkinIssues: String?
+
+    var mainConcern: String?
+    var wantsPhotoUpload: String?
+    var consentShared: Bool
+
+    init(
+        skinFeeling: String? = nil,
+        productReaction: String? = nil,
+        flakiness: String? = nil,
+        diagnosedCondition: String? = nil,
+        hasAllergies: String? = nil,
+        allergiesDetails: String = "",
+        hasSkinIssues: String? = nil,
+        mainConcern: String? = nil,
+        wantsPhotoUpload: String? = nil,
+        consentShared: Bool = false
+    ) {
+        self.skinFeeling = skinFeeling
+        self.productReaction = productReaction
+        self.flakiness = flakiness
+        self.diagnosedCondition = diagnosedCondition
+        self.hasAllergies = hasAllergies
+        self.allergiesDetails = allergiesDetails
+        self.hasSkinIssues = hasSkinIssues
+        self.mainConcern = mainConcern
+        self.wantsPhotoUpload = wantsPhotoUpload
+        self.consentShared = consentShared
+    }
+}
