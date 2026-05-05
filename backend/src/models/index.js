48e93607fb76926ce593f685b430da1e645f077f
Soulaimane Saadi <146191762+SoulaimaneSaadi@users.noreply.github.com>
2026-05-05 11:47:42 +0200

    feat(models): add SkinFormAnswer to User model

--- backend/src/models/index.js
+++ backend/src/models/index.js
@@ -3,6 +3,7 @@ const sequelize = require("../config/database");
 const User = require("./User");
 const DermatologistProfile = require("./DermatologistProfile");
 const SkinAnalysis = require("./SkinAnalysis");
+const SkinFormAnswer = require("./SkinFormAnswer");
 
 User.hasOne(DermatologistProfile, {
   foreignKey: "user_id",
@@ -26,9 +27,15 @@ SkinAnalysis.belongsTo(User, {
     as: "user",
 });
 
+SkinFormAnswer.belongsTo(User, {
+  foreignKey: "user_id",
+  as: "user",
+});
+
 module.exports = {
   sequelize,
   User,
   DermatologistProfile,
-  SkinAnalysis
+  SkinAnalysis,
+  SkinFormAnswer,
 };
