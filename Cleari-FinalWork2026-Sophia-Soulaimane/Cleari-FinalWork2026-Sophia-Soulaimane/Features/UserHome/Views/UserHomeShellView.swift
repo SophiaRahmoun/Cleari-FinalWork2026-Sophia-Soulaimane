f23a74bed4c00fb2787aaa7587f45ab16308bfa1
Soulaimane Saadi <146191762+SoulaimaneSaadi@users.noreply.github.com>
2026-05-14 15:37:51 +0200

    feat: connect consultationflow to user home navigation

--- Cleari-FinalWork2026-Sophia-Soulaimane/Cleari-FinalWork2026-Sophia-Soulaimane/Features/UserHome/Views/UserHomeShellView.swift
+++ Cleari-FinalWork2026-Sophia-Soulaimane/Cleari-FinalWork2026-Sophia-Soulaimane/Features/UserHome/Views/UserHomeShellView.swift
@@ -5,3 +5,15 @@
 //  Created by admin on 15/02/2026.
 //
 
+import SwiftUI
+
+struct UserHomeShellView: View {
+    var body: some View {
+        ZStack(alignment: .bottom) {
+            FeedView()
+
+            ScanBottomBar()
+        }
+        .navigationBarBackButtonHidden(true)
+    }
+}
