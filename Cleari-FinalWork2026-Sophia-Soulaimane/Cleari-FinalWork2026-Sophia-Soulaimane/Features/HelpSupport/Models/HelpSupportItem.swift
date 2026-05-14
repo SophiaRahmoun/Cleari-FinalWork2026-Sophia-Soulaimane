5b0ff730defbb22c7eb94a6b3838a2a9d52cce71
Soulaimane Saadi <146191762+SoulaimaneSaadi@users.noreply.github.com>
2026-05-14 08:53:16 +0200

    feat: add a model for HelpSupportView

--- Cleari-FinalWork2026-Sophia-Soulaimane/Cleari-FinalWork2026-Sophia-Soulaimane/Features/HelpSupport/Models/HelpSupportItem.swift
+++ Cleari-FinalWork2026-Sophia-Soulaimane/Cleari-FinalWork2026-Sophia-Soulaimane/Features/HelpSupport/Models/HelpSupportItem.swift
@@ -5,3 +5,11 @@
 //  Created by Soulaimane Saadi on 13/05/2026.
 //
 
+import Foundation
+
+struct HelpSupportItem: Identifiable {
+    let id = UUID()
+    let title: String
+    let description: String
+    let iconName: String
+}
