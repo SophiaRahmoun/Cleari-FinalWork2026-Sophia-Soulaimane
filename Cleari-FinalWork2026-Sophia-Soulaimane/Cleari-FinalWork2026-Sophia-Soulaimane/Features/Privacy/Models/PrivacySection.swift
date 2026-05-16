c34152d2182399df644a6407acf119d6ada02590
Soulaimane Saadi <146191762+SoulaimaneSaadi@users.noreply.github.com>
2026-05-16 13:46:30 +0200

    feat: add privacy section model structure

--- Cleari-FinalWork2026-Sophia-Soulaimane/Cleari-FinalWork2026-Sophia-Soulaimane/Features/Privacy/Models/PrivacySection.swift
+++ Cleari-FinalWork2026-Sophia-Soulaimane/Cleari-FinalWork2026-Sophia-Soulaimane/Features/Privacy/Models/PrivacySection.swift
@@ -5,3 +5,10 @@
 //  Created by Soulaimane Saadi on 16/05/2026.
 //
 
+import Foundation
+
+struct PrivacySection: Identifiable {
+    let id = UUID()
+    let title: String
+    let text: String
+}
