45931197a61d09043ee8d3c38f86ddcffaaf6572
Soulaimane Saadi <146191762+SoulaimaneSaadi@users.noreply.github.com>
2026-05-17 13:01:15 +0200

    feat: prepare feed structure for backend integration

---
+++ Cleari-FinalWork2026-Sophia-Soulaimane/Cleari-FinalWork2026-Sophia-Soulaimane/Features/Feed/Models/CommunityPost.swift
@@ -0,0 +1,7 @@
+//
+//  CommunityPost.swift
+//  Cleari-FinalWork2026-Sophia-Soulaimane
+//
+//  Created by Soulaimane Saadi on 17/05/2026.
+//
+
---
+++ Cleari-FinalWork2026-Sophia-Soulaimane/Cleari-FinalWork2026-Sophia-Soulaimane/Features/Feed/Services/CommunityPostService.swift
@@ -0,0 +1,7 @@
+//
+//  CommunityPostService.swift
+//  Cleari-FinalWork2026-Sophia-Soulaimane
+//
+//  Created by Soulaimane Saadi on 17/05/2026.
+//
+
