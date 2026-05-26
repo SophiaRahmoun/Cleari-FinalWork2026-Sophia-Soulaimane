d2a4b893f482c4e6dab532af7c2cf256a7f7d37f
Soulaimane Saadi <146191762+SoulaimaneSaadi@users.noreply.github.com>
2026-05-26 14:53:25 +0200

    feat: add profile back navigation flow

--- Cleari-FinalWork2026-Sophia-Soulaimane/Cleari-FinalWork2026-Sophia-Soulaimane/Features/Profile/User/Views/UserProfileView.swift
+++ Cleari-FinalWork2026-Sophia-Soulaimane/Cleari-FinalWork2026-Sophia-Soulaimane/Features/Profile/User/Views/UserProfileView.swift
@@ -8,11 +8,26 @@
 import SwiftUI
 
 struct UserProfileView: View {
+    @Environment(\.dismiss) private var dismiss
     @State private var showPayementView = false
     var body: some View {
         ZStack {
             DarkBackground()
             VStack(spacing: 0) {
+                HStack {
+                    Button {
+                        dismiss()
+                    } label: {
+
+                        Image(systemName: "chevron.left")
+                            .font(.system(size: 24, weight: .bold))
+                            .foregroundColor(.white)
+                    }
+                    Spacer()
+                }
+                .padding(.horizontal, 28)
+                .padding(.top, 20)
+
                 ScrollView(showsIndicators: false) {
 
                     VStack(spacing: 32) {
@@ -23,6 +38,7 @@ struct UserProfileView: View {
                             username: "Annedupont",
                             memberSince: "2026"
                         )
+
                         VStack(spacing: 10) {
 
                             ProfileMenuSection(title: "Account")
@@ -56,10 +72,9 @@ struct UserProfileView: View {
                         }
                         .padding(.horizontal, 32)
                     }
-                    .padding(.top, 45)
+                    .padding(.top, 25)
                     .padding(.bottom, 35)
                 }
-
                 ScanBottomBar()
             }
         }
