988ce6b6c5f65511ccbbc3485b404fcbc906a578
Soulaimane Saadi <146191762+SoulaimaneSaadi@users.noreply.github.com>
2026-05-26 14:07:20 +0200

    feat: add subscription access from profile page

--- Cleari-FinalWork2026-Sophia-Soulaimane/Cleari-FinalWork2026-Sophia-Soulaimane/Features/Profile/User/Views/UserProfileView.swift
+++ Cleari-FinalWork2026-Sophia-Soulaimane/Cleari-FinalWork2026-Sophia-Soulaimane/Features/Profile/User/Views/UserProfileView.swift
@@ -8,36 +8,51 @@
 import SwiftUI
 
 struct UserProfileView: View {
+    @State private var showPayementView = false
     var body: some View {
         ZStack {
             DarkBackground()
-
             VStack(spacing: 0) {
                 ScrollView(showsIndicators: false) {
+
                     VStack(spacing: 32) {
+
                         ProfileHeader(
                             imageName: "ProfileSample",
                             fullName: "Anne Dupont",
                             username: "Annedupont",
                             memberSince: "2026"
                         )
-
                         VStack(spacing: 10) {
+
                             ProfileMenuSection(title: "Account")
                             ProfileMenuRow(title: "Edit profile")
-                            ProfileMenuRow(title: "Subscription")
+
+                            Button {
+
+                                if TokenStorage.shared.userRole != "dermatologist" {
+                                    showPayementView = true
+                                }
+
+                            } label: {
+
+                                ProfileMenuRow(title: "Subscription")
+                            }
+                            .buttonStyle(.plain)
 
                             ProfileMenuSection(title: "My skin")
                             ProfileMenuRow(title: "Skin goals")
                             ProfileMenuRow(title: "My skin scans")
-
                             ProfileMenuSection(title: "Settings")
                             ProfileMenuRow(title: "Appointments")
                             ProfileMenuRow(title: "Privacy  & security")
                             ProfileMenuRow(title: "Help & support")
                             ProfileMenuRow(title: "Language")
                             ProfileMenuRow(title: "Notifications")
-                            ProfileMenuRow(title: "Log out", isDestructive: true)
+                            ProfileMenuRow(
+                                title: "Log out",
+                                isDestructive: true
+                            )
                         }
                         .padding(.horizontal, 32)
                     }
@@ -47,6 +62,10 @@ struct UserProfileView: View {
 
                 ScanBottomBar()
             }
+        }
+        .fullScreenCover(isPresented: $showPayementView) {
+
+            PayementView()
         }
     }
 }
